ba1c3c549da2e5fae0108206601f946172a779ac
sam123114 <samuelbou2000@gmail.com>
2020-12-12 22:58:16 -0500

    Added graves on player death

--- Survival+/src/ca/wartog/survivalplus/Main.java
+++ Survival+/src/ca/wartog/survivalplus/Main.java
@@ -1,12 +1,24 @@
 package ca.wartog.survivalplus;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import ca.wartog.survivalplus.listeners.Events;
 
 public class Main extends JavaPlugin{
 	
+	private static Main instance;
+	
 	@Override
 	public void onEnable() {
+		instance = this;
 		
+		//init events
+		Bukkit.getPluginManager().registerEvents(new Events(), this);
+	}
+	
+	public static Main getInstance() {
+		return instance;
 	}
 
 }
---
+++ Survival+/src/ca/wartog/survivalplus/listeners/Events.java
@@ -0,0 +1,72 @@
+package ca.wartog.survivalplus.listeners;
+
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Chest;
+import org.bukkit.block.Sign;
+import org.bukkit.block.data.type.Chest.Type;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class Events implements Listener{
+	
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent e) {
+		Player p = e.getEntity();
+		Location loc = p.getLocation();
+		List<ItemStack> drops = e.getDrops();
+		
+		double x = loc.getX();
+		double y = loc.getY();
+        
+        Location x1 = loc.clone();
+        Location x2 = loc.clone();
+        Location x3 = loc.clone();
+     
+        x2.setX(x + 1);
+        x3.setY(y + 1);
+     
+        Block block1 = x1.getBlock();
+        Block block2 = x2.getBlock();
+        Block block3 = x3.getBlock();
+        
+        org.bukkit.block.data.type.Chest chestData1;
+        org.bukkit.block.data.type.Chest chestData2;
+        
+        if(drops.size() < 27) {
+        	block1.setType(Material.CHEST);
+        } else {
+        	block1.setType(Material.CHEST);
+        	block2.setType(Material.CHEST);
+        	
+        	Chest chest1 = (Chest) block1.getState();
+        	Chest chest2 = (Chest) block2.getState();
+        	
+        	chestData1 = (org.bukkit.block.data.type.Chest) chest1.getBlockData();
+        	chestData2 = (org.bukkit.block.data.type.Chest) chest2.getBlockData();
+        	
+        	chestData1.setType(Type.LEFT);
+        	block1.setBlockData(chestData1, true);
+        	chestData2.setType(Type.RIGHT);
+        	block2.setBlockData(chestData2, true);
+        }
+        
+        Chest bChest = (Chest) x1.getBlock().getState();
+        bChest.getInventory().setContents(drops.toArray(new ItemStack[drops.size()]));
+        e.getDrops().clear();
+        
+        block3.setType(Material.OAK_SIGN);
+        Sign sign = (Sign) x3.getBlock().getState();
+        sign.setLine(1, p.getName() + "'s grave");
+        sign.update();
+        
+        p.sendMessage("§6You died at " + "X: §c" + loc.getBlockX() + "§6 Y: §c" + loc.getBlockY() + "§6 Z: §c" + loc.getBlockZ());
+	}
+	
+}
